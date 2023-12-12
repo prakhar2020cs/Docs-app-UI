5be67a1bf1be01e90319a77986d04d2306e78579
prakhar2020cs <prakhar06035@gmail.com>
2023-12-12 19:43:13 +0530

    without framer

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
 import { FaFile } from "react-icons/fa";
 import { FaDownload } from "react-icons/fa";
+import { IoCloseSharp } from "react-icons/io5";
+import { motion } from "framer-motion"
 
-function Card() {
+function Card({data}) {
   return (
-    <div className='relative w-60 h-72 rounded-[40px] bg-zinc-900/60 text-white px-5 py-10 overflow-hidden'>
+    <div className='relative flex-shrink-0 w-60 h-72 rounded-[40px] bg-zinc-900/60 text-white px-8 py-10 overflow-hidden'>
         <FaFile/> 
-        <p className='text-sm leading-tight mt-5 font-semibold'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam?</p>
-         <div className='footer absolute bottom-0 w-full py-3 px-8 left-0'>
-            <div className='flex items-center justify-between mb-5' >
-                <h5>.4mb</h5>
+        <p className='text-sm leading-tight p-3 mt-5 font-semibold'>{data.desc}</p>
+         <div className='footer absolute bottom-0 w-full left-0'>
+            <div className='flex items-center justify-between px-8 mb-3' >
+                <h5>{data.filesize}</h5>
                 <span className='w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center'>
-                    <FaDownload size=".7em" color="#fff" />
+                    {data.close ? <IoCloseSharp />:<FaDownload size=".7em" color="#fff" />}
+                    
                 </span>
             </div>
+            { 
+                data.tag.isOpen && (
+                 <div className={`tag w-full py-4 ${data.tag.tagColor=="blue"?"bg-blue-600":"bg-green-600"} flex items-center justify-center`}>
+                 <h3 className='text-sm font-semibold'>{data.tag.tagTitle}</h3>
+                 </div>
+                  ) 
+            }
+           
             </div>
           </div> 
       
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -2,10 +2,35 @@ import React from 'react'
 import Card from './Card'
 
 function Foreground() {
+   
+
+    const data=[
+        {
+            desc:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam?",
+            filesize: ".9mb",
+            close:true,
+            tag:{isOpen:false,tagTitle:"Download Now",tagColor:"green"}
+        },
+        {
+            desc:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam?",
+            filesize: ".9mb",
+            close:false,
+            tag:{isOpen:true,tagTitle:"Download!!",tagColor:"blue"}
+        },
+        {
+            desc:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam?",
+            filesize: ".5mb",
+            close:true,
+            tag:{isOpen:true,tagTitle:"Download Now",tagColor:"green"}
+        },
+    ];
+    
   return (
     
-        <div className='fixed top-0 left-0 z-[3] w-full h-full'>
-         <Card/>
+        <div className='fixed top-0 flex left-0 z-[3] w-full gap-4 h-full flex-wrap'>
+         {data.map((item,index)=>(
+            <Card data={item} />
+         ))}
         </div>
     
   )
